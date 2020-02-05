0097e3e08fa2c6e79fa8173f76d82d22afe34ba1
Yu Liu <942290857@qq.com>
2020-02-04 19:15:43 -0600

    Add files via upload
    init files

---
+++ hw1/q5/Frequency.java
@@ -0,0 +1,14 @@
+package q5;
+
+import java.util.concurrent.Callable;
+
+public class Frequency implements Callable<Integer> {
+    public static int parallelFreq(int x, int[] A, int numThreads){
+        //your implementation goes here, return -1 if the input is not valid.
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+}
