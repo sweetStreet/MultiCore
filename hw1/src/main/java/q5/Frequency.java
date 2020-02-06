685c042c62d933b5b967fad645fabe246fa88763
sweetStreet <942290857@qq.com>
2020-02-05 20:58:30 -0600

    convert to maven
    convert to maven

--- hw1/src/main/java/q5/Frequency.java
+++ hw1/src/main/java/q5/Frequency.java
@@ -5,6 +5,7 @@ import java.util.concurrent.Callable;
 public class Frequency implements Callable<Integer> {
     public static int parallelFreq(int x, int[] A, int numThreads){
         //your implementation goes here, return -1 if the input is not valid.
+        return -1;
     }
 
     //changed by Yizhuo
